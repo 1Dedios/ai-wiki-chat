04f03210619bbf745f4ae26e2b1f78f614cfdfe1
Dedios <dediosprogramming@gmail.com>
2025-01-15 18:47:00 -0500

    main.py: created

---
+++ main.py
@@ -0,0 +1,17 @@
+# import all modules
+# import package for pysimplegui for user input
+
+
+
+'''
+
+response from llm should appear in this section
+
+'''
+
+
+'''
+
+user input element here - extract value and send to parser module
+
+'''
